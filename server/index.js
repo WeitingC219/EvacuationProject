fcf54b7a6861f6fd3a5aba894c4a723791693401
Weiting Chien <r06521604@caece.net>
2020-02-03 16:48:21 +0800

    modify db config

--- server/index.js
+++ server/index.js
@@ -21,12 +21,12 @@ app.use((req, res, next) => {
 app.use('/', sensor);
 
 // local db 請改 mongoose.connect("mongodb://localhost:27017/<db_name>"）
-// altas db 請將 @cluster0-9megj.mongodb.net改成自己cluster，並在nodemon改自己的user_name,user_password,db_name
+// altas db 請將 @clusterxxxxxx.mongodb.net改成自己cluster，並在nodemon改自己的user_name,user_password,db_name
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${
       process.env.MONGO_PASSWORD
-    }@cluster0-9megj.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    }@clusterxxxxxx.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
   ).then(() => {
     console.log('Connection Mongodb successful');
     app.listen(8000);
